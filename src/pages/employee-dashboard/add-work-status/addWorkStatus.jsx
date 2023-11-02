c47f83b19b2147868df2f4cd0cffead5109cf284
Samiul-Ahmed-96 <samiulahmedanik45@gmail.com>
2023-11-02 19:43:31 +0600

    Added Style

--- src/pages/employee-dashboard/add-work-status/addWorkStatus.jsx
+++ src/pages/employee-dashboard/add-work-status/addWorkStatus.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import './add-work-status.scss';
 
 const addWorkStatus = () => {
-
+  const { handleSubmit, control, register } = useForm();
+  const onSubmit = (data) => {
+    console.log(data); // You can process the form data here
+  };
   return (
     <div className="table-wrapper-content">
-    {/* <div className="container"> */}
+      {/* <div className="container"> */}
       <div className="row">
         <div className="col-md-12">
           <div className="heading">
@@ -13,12 +18,49 @@ const addWorkStatus = () => {
               <i class="fa-regular fa-square-plus"></i>
             </h2>
           </div>
-         
+          <div className="add-form">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="row">
+                <div className="col-md-6">
+                  <label>Task / Project</label>
+                  <input {...register("task")} />
+                </div>
+                <div className="col-md-6">
+                  <label>Date of Report</label>
+                  <input type="date" {...register("date")} />
+                </div>
+               
+                <div className="col-md-6">
+                  <label for="hour">Hours Worked</label>
+                  <input type="number" {...register("hour")} />
+                </div>
+                
+                <div className="col-md-6">
+                  <label for="hour">Work Status</label>
+                  <select {...register("work-status")}>
+                    <option value="complete">Complete</option>
+                    <option value="in progress">In Progress</option>
+                  </select>
+                </div>
+
+                <div className="col-md-12">
+                  <label for="description">Work Description</label>
+                  <textarea {...register("description")} />
+                </div>
+                <div className="col-md-6">
+                <button className="submit-btn">Add Work Status </button>
+                </div>
+
+              
+       
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    {/* </div> */}
-  </div>
-  )
-}
+      {/* </div> */}
+    </div>
+  );
+};
 
-export default addWorkStatus
+export default addWorkStatus;
