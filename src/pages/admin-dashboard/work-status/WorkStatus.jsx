2aa5776c0f478add4623a7c907dffd9923ca34b2
Samiul-Ahmed-96 <samiulahmedanik45@gmail.com>
2023-11-03 16:44:10 +0600

    Added Work status

--- src/pages/admin-dashboard/work-status/WorkStatus.jsx
+++ src/pages/admin-dashboard/work-status/WorkStatus.jsx
@@ -1,9 +1,149 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import userImg from "../../../assets/employee.jpg";
 
 const WorkStatus = () => {
+  const workStatus = [
+    {
+      id: 1,
+      image: userImg,
+      task: "develop ui",
+      date: "22-11-23",
+      hours: "2",
+      status: "in progress",
+      description:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, perferendis amet. Aperiam, esse obcaecati! Placeat adipisci similique modi cum neque.",
+    },
+    {
+      id: 1,
+      image: userImg,
+      task: "develop ui",
+      date: "22-11-23",
+      hours: "2",
+      status: "in progress",
+      description:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, perferendis amet. Aperiam, esse obcaecati! Placeat adipisci similique modi cum neque.",
+    },
+    {
+      id: 1,
+      image: userImg,
+      task: "develop ui",
+      date: "22-11-23",
+      hours: "2",
+      status: "in progress",
+      description:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, perferendis amet. Aperiam, esse obcaecati! Placeat adipisci similique modi cum neque.",
+    },
+    {
+      id: 1,
+      image: userImg,
+      task: "develop ui",
+      date: "22-11-23",
+      hours: "2",
+      status: "in progress",
+      description:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, perferendis amet. Aperiam, esse obcaecati! Placeat adipisci similique modi cum neque.",
+    },
+    {
+      id: 1,
+      image: userImg,
+      task: "develop ui",
+      date: "22-11-23",
+      hours: "2",
+      status: "in progress",
+      description:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, perferendis amet. Aperiam, esse obcaecati! Placeat adipisci similique modi cum neque.",
+    },
+    {
+      id: 1,
+      image: userImg,
+      task: "develop ui",
+      date: "22-11-23",
+      hours: "2",
+      status: "in progress",
+      description:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, perferendis amet. Aperiam, esse obcaecati! Placeat adipisci similique modi cum neque.",
+    },
+    {
+      id: 1,
+      image: userImg,
+      task: "develop ui",
+      date: "22-11-23",
+      hours: "2",
+      status: "in progress",
+      description:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, perferendis amet. Aperiam, esse obcaecati! Placeat adipisci similique modi cum neque.",
+    },
+    {
+      id: 1,
+      image: userImg,
+      task: "develop ui",
+      date: "22-11-23",
+      hours: "2",
+      status: "in progress",
+      description:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, perferendis amet. Aperiam, esse obcaecati! Placeat adipisci similique modi cum neque.",
+    },
+  ];
   return (
-    <div>WorkStatus</div>
-  )
-}
+    <div className="content-wrapper">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="heading">
+            <h2>
+              <span>Work Status</span> <i class="fa-solid fa-battery-half"></i>{" "}
+            </h2>
+          </div>
+          <table class="table-modify table table-striped">
+            <thead>
+              <tr>
+                <th>Employee</th>
+                <th className="task">Task</th>
+                <th>Date</th>
+                <th>Hours Of Work</th>
+                <th>Status</th>
+                <th className="description">Desciption</th>
+                <th className="action-area">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {workStatus.map((work) => (
+                <tr>
+                  <td>
+                    <img
+                      className="employee-img"
+                      src={work?.image}
+                      alt="employee"
+                    />
+                  </td>
+                  <td>{work?.task}</td>
+                  <td>{work?.date}</td>
+                  <td>{work?.hours}</td>
+                  <td>{work?.status}</td>
+                  <td>{work?.description}</td>
+                  <td>
+                    <Link to={`update-work-status/${work.id}`}>
+                      <button className="update-btn">
+                        {" "}
+                        <i className="far fa-edit"></i>
+                      </button>
+                    </Link>
 
-export default WorkStatus
+                    <button
+                      // onClick={() => handleDelete(employee.id)}
+                      className="delete-btn"
+                    >
+                      <i className="fas fa-trash-alt"></i>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkStatus;
--- src/pages/employee-dashboard/profile/Profile.jsx
+++ src/pages/employee-dashboard/profile/Profile.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
-
+import React from "react";
+import userImg from "../../../assets/employee.jpg";
+import "./profile.scss";
 const Profile = () => {
+  const user = {
+    id: 9,
+    img: userImg,
+    name: "Sarah Smith",
+    designation: "Software QA Engineer",
+    mobile: "+880-1634534522",
+    email: "sara.smith@gmail.com",
+    address: "Dhanmondi , Dhaka",
+    joiningDate: "01-06-22",
+  };
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="content-wrapper">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="heading">
+            <h2>
+              <span>Profile</span> <i class="fa-solid fa-user-tie"></i>
+            </h2>
+          </div>
+          <div className="profile-current-task-wrapper">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="profile-content-wrapper">
+                  <div className="row">
+                    <div className="col-md-5">
+                      <div className="user-img">
+                        <img src={userImg} alt="employee" />
+                      </div>
+                    </div>
+                    <div className="col-md-7">
+                      <div className="user-info">
+                        <div className="row">
+                          <div className="col-md-12">
+                            <h2>{user.name}</h2>
+                          </div>
+                          <div className="col-md-12">
+                            <h3>{user.designation}</h3>
+                          </div>
+                          <div className="col-md-12">
+                            <h3><i class="fa-solid fa-mobile-screen-button"></i> <span>{user.mobile}</span></h3>
+                          </div>
+                          <div className="col-md-12">
+                            <h3><i class="fa-solid fa-envelope-open-text"></i> <span>{user.email}</span></h3>
+                          </div>
+                          <div className="col-md-12">
+                            <h3><i class="fa-solid fa-calendar-days"></i> <span>{user.joiningDate}</span></h3>
+                          </div>
+                          <div className="col-md-12">
+                            <h3><i class="fa-solid fa-location-dot"></i> <span>{user.address}</span></h3>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="current-task-wrapper"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
